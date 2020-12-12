c061577f2f6a65520beffc9a53a8b5f996eb10d7
daniel alexander vasquez cazon <a2019115093@estudiantes.upsa.edu.bo>
2020-12-12 10:59:32 -0400

    Se realizo el controlador

--- FinalProgramacion/FinalProgramacion/Controllers/Numerito.cs
+++ FinalProgramacion/FinalProgramacion/Controllers/Numerito.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FinalProgramacion.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,6 +12,22 @@ namespace FinalProgramacion.Controllers
     [ApiController]
     public class Numerito : ControllerBase
     {
+        public Numero operaciones(int numero)
+        {
+            string texto = "";
+            
 
+            if (numero < 0) { texto = "ERROR"; }
+            if (numero == 0) { texto = "REALIZADO POR DANIEL ALEXANDER VASQUEZ CAZON"; }
+            if (numero > 0) { texto = "https://image.freepik.com/vector-gratis/numeros-cartel-imagen_1639-6453.jpg"; }
+        
+            Numero numeromodel = new Numero
+            {
+                numero = numero,
+                mensaje = texto,
+            };
+         return numeromodel;
+        }
+        
     }
 }
