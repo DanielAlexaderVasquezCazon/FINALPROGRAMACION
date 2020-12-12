6479d4927080859bab22a754ac2111068eea3d0b
daniel alexander vasquez cazon <a2019115093@estudiantes.upsa.edu.bo>
2020-12-12 10:51:00 -0400

    se agrego el modelo

---
+++ FinalProgramacion/FinalProgramacion/Models/Numero.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProgramacion.Models
+{
+    public class Numero
+    {
+        [Key]
+        [Required]
+        public int numero { get; set; }
+        [Required]
+        public string mensaje { get; set; }
+
+    }
+}
